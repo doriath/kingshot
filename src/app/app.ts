c26af3fa39205de20906c379ddca230ef1c3d7d8
tomasz.zurkowski <tomasz.zurkowski@gmail.com>
2025-09-28 13:30:29 +0000

    Proper sidebar navigation.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterLink, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('myapp');
+
+  isSideNavOpen = signal(false);
+
+  toggleSideNav() {
+    this.isSideNavOpen.set(!this.isSideNavOpen());
+  }
 }
