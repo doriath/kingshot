51651e096332671911bad5b9b851b4852c7ff786
tomasz.zurkowski <tomasz.zurkowski@gmail.com>
2025-11-12 08:46:36 +0000

    Fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { BEAR_ROUTES } from './bear.routes';
-import { VIKINGS_ROUTES } from './vikings.routes';
-import { SWORDLAND_ROUTES } from './swordland.routes';
 import { MISC_ROUTES } from './misc.routes';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent, title: 'Home' },
     ...BEAR_ROUTES,
-    ...VIKINGS_ROUTES,
-    ...SWORDLAND_ROUTES,
     ...MISC_ROUTES,
 ];
--- src/app/misc.routes.ts
+++ src/app/misc.routes.ts
@@ -1,19 +1,7 @@
 import { Routes } from '@angular/router';
-import { BearSimulatorComponent } from './bear-simulator/bear-simulator';
-import { MyResearchComponent } from './my-research/my-research';
 import { HeroGearComponent } from './hero-gear/hero-gear';
 
 export const MISC_ROUTES: Routes = [
-    {
-        path: 'bear-simulator',
-        component: BearSimulatorComponent,
-        title: 'Bear Simulator',
-    },
-    {
-        path: 'my-research',
-        component: MyResearchComponent,
-        title: 'My Research',
-    },
     {
         path: 'hero-gear',
         component: HeroGearComponent,
