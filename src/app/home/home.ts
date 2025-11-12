e1b0c3a8b093a5356b870a5f97f9322db79013c0
tomasz.zurkowski <tomasz.zurkowski@gmail.com>
2025-11-12 08:18:21 +0000

    Improve the home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
   changeDetection: ChangeDetectionStrategy.OnPush
