7b5ac84dfd6e068ca9c029385e066209cc24645f
tomasz.zurkowski <tomasz.zurkowski@gmail.com>
2025-11-16 16:52:26 +0000

    Add swordland page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { BEAR_ROUTES } from './bear.routes';
 import { MISC_ROUTES } from './misc.routes';
+import { SwordlandEventComponent } from './swordland-event/swordland-event';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent, title: 'Home' },
+    { path: 'swordland', component: SwordlandEventComponent, title: 'Swordland Event' },
     ...BEAR_ROUTES,
     ...MISC_ROUTES,
 ];
