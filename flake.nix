34fd55026545b9fd8331956f9a1418fd6b58a232
Tomasz Zurkowski <tomasz.zurkowski@gmail.com>
2025-11-20 19:58:11 +0100

    Make the repo work outside of Firebase studio.

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Flake for kingshot";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.stable.latest.default.override { targets = [ "wasm32-unknown-unknown" ]; };
+        rustPlatform = pkgs.recurseIntoAttrs (
+          pkgs.makeRustPlatform {
+            rustc = rust;
+            cargo = rust;
+          }
+        );
+      in
+      rec {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.bashInteractive
+            pkgs.rust-analyzer
+            pkgs.nodejs_20
+            pkgs.wasm-pack
+            pkgs.gcc
+            rust
+          ];
+        };
+      }
+    );
+}
